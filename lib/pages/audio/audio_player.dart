d4965ecf1d6345254b3eea50ea142496c04ae4e4
alermi <alermi@ucsd.edu>
2021-08-20 22:06:05 -0700

    Added audio player to the bottom of the map

---
+++ lib/pages/audio/audio_player.dart
@@ -0,0 +1,74 @@
+import 'package:audio_video_progress_bar/audio_video_progress_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:tour_guide/pages/audio/audio_page_manager.dart';
+
+class AudioPlayerView extends StatefulWidget {
+  const AudioPlayerView({Key? key}) : super(key: key);
+
+  @override
+  _AudioPlayerViewState createState() => _AudioPlayerViewState();
+}
+
+class _AudioPlayerViewState extends State<AudioPlayerView> {
+  late final AudioPageManager _pageManager;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageManager = AudioPageManager();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        children: [
+          ValueListenableBuilder<ProgressBarState>(
+            valueListenable: _pageManager.progressNotifier,
+            builder: (_, value, __) {
+              return ProgressBar(
+                progress: value.current,
+                buffered: value.buffered,
+                total: value.total,
+                onSeek: _pageManager.seek,
+              );
+            },
+          ),
+          ValueListenableBuilder<ButtonState>(
+            valueListenable: _pageManager.buttonNotifier,
+            builder: (_, value, __) {
+              switch (value) {
+                case ButtonState.loading:
+                  return Container(
+                    margin: EdgeInsets.all(8.0),
+                    width: 32.0,
+                    height: 32.0,
+                    child: CircularProgressIndicator(),
+                  );
+                case ButtonState.paused:
+                  return IconButton(
+                    icon: Icon(Icons.play_arrow),
+                    iconSize: 32.0,
+                    onPressed: _pageManager.play,
+                  );
+                case ButtonState.playing:
+                  return IconButton(
+                    icon: Icon(Icons.pause),
+                    iconSize: 32.0,
+                    onPressed: _pageManager.pause,
+                  );
+              }
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _pageManager.dispose();
+    super.dispose();
+  }
+}
--- lib/pages/map.dart
+++ lib/pages/map.dart
@@ -10,22 +10,20 @@ class MapPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: GoogleMap(
-        mapType: MapType.hybrid,
-        myLocationEnabled: true,
-        minMaxZoomPreference: MinMaxZoomPreference(16, 20),
-        cameraTargetBounds: CameraTargetBounds(
-          LatLngBounds(
-            northeast: LatLng(41.010407, 28.981304),
-            southwest: LatLng(41.004685, 28.976260),
-          ),
+    return GoogleMap(
+      mapType: MapType.hybrid,
+      myLocationEnabled: true,
+      minMaxZoomPreference: MinMaxZoomPreference(16, 20),
+      cameraTargetBounds: CameraTargetBounds(
+        LatLngBounds(
+          northeast: LatLng(41.010407, 28.981304),
+          southwest: LatLng(41.004685, 28.976260),
         ),
-        onMapCreated: _onMapCreated,
-        initialCameraPosition: CameraPosition(
-          target: LatLng(41.007719, 28.979164),
-          zoom: 17.0,
-        ),
+      ),
+      onMapCreated: _onMapCreated,
+      initialCameraPosition: CameraPosition(
+        target: LatLng(41.007719, 28.979164),
+        zoom: 17.0,
       ),
     );
   }
